5458ace4afac97057fab68c6510d58f14d2e7048
Dito Bayu Adhitya <ditobayuzs1@gmail.com>
2023-02-25 12:21:11 +0700

    fix dark mode

--- src/pages/challenge/index.js
+++ src/pages/challenge/index.js
@@ -34,9 +34,9 @@ const Challenge = () => {
 
       <div
         className={`flex duration-300 flex-col bg-slate-100 gap-1 dark:bg-slate-900 sm:pr-4 pt-4
-        ${isSidebarFull ? " sm:pl-64" : " sm:pl-24"}
-        px-1
-          w-screen h-full`}
+          ${isSidebarFull ? " sm:pl-64" : " sm:pl-24"}
+          px-1
+            w-screen h-full`}
       >
         <div className={`${!selectedChallenge ? "flex" : "hidden"} flex-col`}>
           <div className="text-xl md:text-3xl font-bold p-4">
@@ -58,7 +58,7 @@ const Challenge = () => {
                   val.participant.find((val) => val.id === userData.user._id)
                     ? "hover:scale-105"
                     : ""
-                } h-full w-64 md:w-96 rounded-3xl dark:bg-slate-800 duration-200`}
+                } h-full w-64 md:w-96 rounded-3xl bg-white shadow-md dark:bg-slate-800 duration-200`}
               ></button>
               <div className="flex flex-col pt-4 px-8 flex-1 justify-between">
                 <div className="text-lg md:text-3xl ">{val.title}</div>
@@ -105,7 +105,7 @@ const Challenge = () => {
             <div className="m-4 flex items-center flex-row">
               <button
                 onClick={() => setSelectedChallenge("")}
-                className="h-8 w-8 flex items-center justify-center rounded-full mr-4 dark:bg-slate-800"
+                className="h-8 w-8 flex items-center justify-center rounded-full mr-4 bg-white shadow-md dark:bg-slate-800"
               >
                 {"<"}
               </button>
@@ -115,7 +115,7 @@ const Challenge = () => {
             </div>
             <button
               onClick={() => setChallengePage("leaderboard")}
-              className="flex flex-col overflow-hidden mr-8 rounded-xl justify-center hover:scale-105 duration-200 items-center h-full dark:bg-slate-800"
+              className="flex flex-col overflow-hidden mr-8 rounded-xl justify-center hover:scale-105 duration-200 items-center h-full bg-white shadow-md dark:bg-slate-800"
             >
               <div className="mx-4 my-2">Leaderboard</div>
               <div className="h-1 w-full bg-cyan-400"></div>
@@ -124,7 +124,7 @@ const Challenge = () => {
           <div className="flex flex-col md:flex-row h-full overflow-hidden noScrollbar overflow-y-scroll">
             <div className="w-full md:w-8/12">
               <iframe
-                className="rounded-lg md:rounded-[40px] h-52 sm:h-80 md:h-[500px]"
+                className="rounded-lg md:rounded-[40px] h-52 sm:h-80 md:h-[400px]"
                 width="100%"
                 height="100%"
                 src={
@@ -147,7 +147,7 @@ const Challenge = () => {
                     : null
                   : selectedVideo.title}
               </div>
-              <div className="rounded-3xl h-52 md:h-96 bg-slate-800 p-4 md:p-8 mb-4">
+              <div className="rounded-3xl h-52 md:h-96 bg-white dark:bg-slate-800 p-4 md:p-8 mb-4">
                 <div>Catatan</div>
                 {/* <div className=" h-full"> */}
                 <textarea
@@ -155,19 +155,19 @@ const Challenge = () => {
                   className="mt-2 p-4 noScrollbar h-28 md:h-64 w-full bg-transparent border rounded-xl border-slate-600"
                 ></textarea>
                 <div className="flex flex-row-reverse">
-                  <button className="rounded-xl text-sm md:text-md bg-slate-600 py-1 md:py-2 hover:scale-105 duration-200 px-4">
+                  <button className="rounded-xl text-sm md:text-md bg-slate-200 dark:bg-slate-600 py-1 md:py-2 hover:bg-green-300 duration-200 px-4">
                     Save
                   </button>
                 </div>
                 {/* </div> */}
               </div>
             </div>
-            <div className="w-full md:w-4/12 md:px-8 mb-16">
+            <div className="w-full md:w-4/12 md:px-4 mb-16">
               {selectedChallenge?.data?.map((val, i) => (
                 <button
                   key={i}
                   onClick={() => setSelectedVideo(val)}
-                  className="hover:scale-105 duration-200 mb-2 md:mb-4 w-full h-24 bg-slate-800 rounded-3xl p-4 px-8 flex flex-row items-center"
+                  className="hover:scale-105 duration-200 mb-2 md:mb-4 w-full h-24 bg-white dark:bg-slate-800 shadow-md rounded-3xl p-4 px-8 flex flex-row items-center"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -207,25 +207,22 @@ const Challenge = () => {
                   setSelectedChallenge("");
                   setChallengePage("materi");
                 }}
-                className="h-8 w-8 flex items-center justify-center rounded-full mr-4 dark:bg-slate-800"
+                className="h-8 w-8 flex items-center justify-center rounded-full mr-4 bg-white shadow-md dark:bg-slate-800"
               >
                 {"<"}
               </button>
-              <h3 className="text-xl flex items-center">
-                {/* {selectedChallenge.title} */}
-                Leaderboard
-              </h3>
+              <h3 className="text-xl flex items-center">Leaderboard</h3>
             </div>
             <button
               onClick={() => setChallengePage("materi")}
-              className="flex flex-col overflow-hidden mr-8 rounded-xl justify-center hover:scale-105 duration-200 items-center h-full dark:bg-slate-800"
+              className="flex flex-col overflow-hidden mr-8 rounded-xl justify-center hover:scale-105 duration-200 items-center h-full bg-white shadow-md dark:bg-slate-800"
             >
               <div className="mx-4 my-2">Materi</div>
               <div className="h-1 w-full bg-cyan-400"></div>
             </button>
           </div>
           <div className="flex flex-col-reverse gap-2 md:gap-0 md:flex-row">
-            <div className="w-full md:w-8/12 flex flex-col rounded-2xl mb-16 sm:mb-4 bg-slate-800 mr-4">
+            <div className="w-full md:w-8/12 flex flex-col rounded-2xl mb-16 sm:mb-4 bg-white dark:bg-slate-800 shadow-md mr-4">
               <div className="flex flex-row w-full p-4">
                 <div className="w-2/12 px-4">Rank</div>
                 <div className="w-7/12 px-4">Nama</div>
@@ -240,7 +237,7 @@ const Challenge = () => {
               ))}
             </div>
             <div className="w-full md:w-4/12">
-              <div className="flex flex-col rounded-xl dark:bg-slate-800 p-4">
+              <div className="flex flex-col rounded-xl shadow-md bg-white dark:bg-slate-800 p-4">
                 <div className="text-xl border-b dark:border-slate-700 mb-2">
                   Statistik Kamu
                 </div>
